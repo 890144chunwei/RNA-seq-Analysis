a15ac4cb6ce06d12ad0b548be9207a8a50b9f642
Chun-Wei Chen <86547379+890144chunwei@users.noreply.github.com>
2023-01-19 10:32:32 -0600

    Update PartI-master.sh

--- PartI-master.sh
+++ PartI-master.sh
@@ -9,16 +9,16 @@
 pwd; hostname; date
 
 GENOMDIR="/storage/goodell/home/chunweic/mm10/STARgenome"
-FASTQDIR="/storage/goodell/projects/chunweic/220601_Chunwei_RNA_Trx/"
+FASTQDIR="/storage/goodell/projects/chunweic/220601_Chunwei_RNA_Trx"
 
-STAR --genomeDir $GENOMDIR/ --runMode alignReads --runThreadN 10 --readFilesIn $FASTQDIR/HSPC_germline_WT1_R1.fastq $FASTQDIR/HSPC_germline_WT1_R2.fastq \
---outFileNamePrefix $FASTQDIR/HSPC_germline_WT1 --outSAMtype BAM Unsorted --outSAMunmapped Within \
---limitGenomeGenerateRAM 1700000000000;
-
-samtools sort $FASTQDIR/Tcell_WT3Aligned.out.bam $FASTQDIR/HSPC_germline_WT1
-
-featureCounts -T 7 -p -s 2 -t exon -a $GENOMDIR/gencode.vM28.chr_patch_hapl_scaff.annotation.gtf -o $FASTQDIR/HSPC_germline_WT1_fc.txt \
-$FASTQDIR/HSPC_germline_WT1_sort.bam
-
-samtools index $FASTQDIR/HSPC_germline_WT1_sort.bam
-bamCoverage -b $FASTQDIR/HSPC_germline_WT1_sort.bam -bl $HOMEDIR/mm10-blacklist.v2.bed -o $FASTQDIR/Coverage/HSPC_germline_WT1_cov.bw --normalizeUsing RPKM
+for FILES in $FASTQDIR/* ;
+do
+  STAR --genomeDir $GENOMDIR/ --runMode alignReads --runThreadN 10 --readFilesIn $FASTQDIR/HSPC_germline_WT1_R1.fastq $FASTQDIR/HSPC_germline_WT1_R2.fastq \
+  --outFileNamePrefix $FASTQDIR/HSPC_germline_WT1 --outSAMtype BAM Unsorted --outSAMunmapped Within \
+  --limitGenomeGenerateRAM 1700000000000;
+  samtools sort $FASTQDIR/Tcell_WT3Aligned.out.bam $FASTQDIR/HSPC_germline_WT1
+  featureCounts -T 7 -p -s 2 -t exon -a $GENOMDIR/gencode.vM28.chr_patch_hapl_scaff.annotation.gtf -o $FASTQDIR/HSPC_germline_WT1_fc.txt \
+  $FASTQDIR/HSPC_germline_WT1_sort.bam
+  samtools index $FASTQDIR/HSPC_germline_WT1_sort.bam
+  bamCoverage -b $FASTQDIR/HSPC_germline_WT1_sort.bam -bl $HOMEDIR/mm10-blacklist.v2.bed -o $FASTQDIR/Coverage/HSPC_germline_WT1_cov.bw --normalizeUsing RPKM ;
+done
